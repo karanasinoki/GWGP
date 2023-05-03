e89a37dabca5e93e70112307dab2929339bd5198
karanasinoki <omiomik4141@gmail.com>
2023-05-03 20:30:25 +0900

    外枠の判定追加

--- Assets/Scenes/script/BulletManager.cs
+++ Assets/Scenes/script/BulletManager.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class BulletManager : MonoBehaviour
 {
-    private int BulletSpeed = 10;
+    private int BulletSpeed = 50;
 
    
     void Update()
     {
         Move();
+        OFFScrean();
     }
 
     private void Move()
     {
         transform.position += new Vector3(BulletSpeed, 0, 0) * Time.deltaTime;
         
+    }
+    //Bullet‚ªÁ‚¦‚éƒvƒƒOƒ‰ƒ€
+    private void OFFScrean()
+    {
+        if(this.transform.position.x>9.0f)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
--- Assets/Scenes/script/PlayerManager.cs
+++ Assets/Scenes/script/PlayerManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     //プレイヤーの移動速度
-    private int MoveSpeed = 8;
+    private int MoveSpeed = 18;
     //ゲームオブジェクトを取得
     public GameObject BulletPrehab;
     public GameObject FiringPosition;
@@ -18,11 +18,12 @@ public class PlayerManager : MonoBehaviour
     private void Move()
     {
         //キーの入力値を取得
-        float x = Input.GetAxis("Horizontal")*MoveSpeed;
-        float y = Input.GetAxis("Vertical")*MoveSpeed;
-        //取得した入力値を反映させる
-        transform.position += new Vector3(x, y, 0) * Time.deltaTime;
+        float movex = Input.GetAxis("Horizontal") * Time.deltaTime*MoveSpeed;
 
+        float movey = Input.GetAxis("Vertical")*Time.deltaTime*MoveSpeed;
+        //取得した入力値を反映させる
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x + movex, -8.5f, 8.5f),
+                                         Mathf.Clamp(transform.position.y + movey, -4.7f, 4.7f));
     }
 
     //スペースを押すと弾が発射される
@@ -37,5 +38,7 @@ public class PlayerManager : MonoBehaviour
         }
         
     }
+
+    
 
 }
